165d71ac2fd71797f49c72db289279681c7ec85d
mhshih <d00142002@ntu.edu.tw>
2014-12-07 23:22:18 +0800

    add cwn_dirty query code

---
+++ cwn_dirty.py
@@ -0,0 +1,25 @@
+from sqlite3 import connect
+conn=connect('cwn_dirty.sqlite')
+c=conn.cursor()
+
+def lookup_pos(lemma_id):
+    c.execute('select * from cwn_pos where cwn_id like "%'+lemma_id+'%"')
+    cwn_pos=c.fetchone()
+    pos=cwn_pos[2]
+    return pos
+
+def lookup_lemma(lemma_id):
+    c.execute('select * from cwn_lemma where lemma_id="'+lemma_id+'"')
+    cwn_lemma=c.fetchone()
+    lemma_type=cwn_lemma[4].encode('utf-8')
+    return lemma_type
+
+def lookup_sense(lemma_id):
+    c.execute('select * from cwn_sense where lemma_id="'+lemma_id+'"')
+    cwn_sense=c.fetchone()
+    sense_def=cwn_sense[2].encode('utf-8')
+    return sense_def
+
+print lookup_pos('080601')
+print lookup_lemma('080601')
+print lookup_sense('080601')
---
+++ test.py
@@ -0,0 +1,4 @@
+#-*-coding:utf-8-*-
+from nltk.corpus import wordnet
+print wordnet.synsets('傷')
+print wordnet.synsets('階級')
