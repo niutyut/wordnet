4015ed0b83c64f1476d9e8381d301124bf701a29
mhshih <d00142002@ntu.edu.tw>
2015-01-20 18:49:51 +0800

    discuss merged

--- distance.py
+++ distance.py
@@ -1,4 +1,3 @@
-#-*-coding:utf8-*-
 from Levenshtein import distance
 
 cwn_dict={}
@@ -22,19 +21,22 @@ for word in cwn_dict:
         print 'cwn:'
         for i in range(len(sense_list)):
             print sense_list[i]
+            pass
 
         print 'moe:'
         for moe_sense in moe_dict[word]:
             print moe_sense
 
-            flag=0
-            for i in range(len(sense_list)):
+            i=flag=0
+            while i<len(sense_list):
+       #     for i in range(len(sense_list)):
                 if distance(sense_list[i],moe_sense)<30:
                     if flag:sense_list.pop(flag)
                     else:flag=i
+                i+=1
 
         print 'merged:'
         for sense in sense_list:
-            print sense
+            print word,',',sense
 
         print
