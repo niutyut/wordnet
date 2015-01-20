6ad278f35603c762441d2882725b8705cbcf6923
mhshih <d00142002@ntu.edu.tw>
2015-01-20 14:24:23 +0800

    sense per word

--- cwn_dirty.py
+++ cwn_dirty.py
@@ -1,32 +1,27 @@
 from sqlite3 import connect
 conn=connect('../cwn_dirty.sqlite')
 c=conn.cursor()
-
 def lookup_pos(lemma_id):
     c.execute('select * from cwn_pos where cwn_id like "%'+lemma_id+'%"')
     cwn_pos=c.fetchone()
     pos=cwn_pos[2]
     return pos
-
 def lookup_lemma(lemma_id):
     c.execute('select * from cwn_lemma where lemma_id="'+lemma_id+'"')
     cwn_lemma=c.fetchone()
     lemma_type=cwn_lemma[4].encode('utf-8')
     return lemma_type
-
 def lookup_sense(lemma_id):
     c.execute('select * from cwn_sense where lemma_id="'+lemma_id+'"')
     cwn_sense=c.fetchone()
     sense_def=cwn_sense[2].encode('utf-8')
     return sense_def
-
 pos='V' # N,V,A,D
 wn_pos='v' # n,v,a,r
-
 # begin with one sense (cwn_id) of a particular POS from cwn_pos table
 #c.execute('select * from cwn_pos where pos = "'+pos+'"')
-c.execute('select * from cwn_pos where pos like "%'+pos+'%"')
-#c.execute('select * from cwn_pos')
+#c.execute('select * from cwn_pos where pos like "%'+pos+'%"')
+c.execute('select * from cwn_pos')
 processed_sense_ids=set()
 for cwn_id,pos_sno,p in c.fetchall():
     sense_id=cwn_id[:8]
@@ -43,7 +38,7 @@ for cwn_id,pos_sno,p in c.fetchall():
         cwn_sense=c.fetchone()
         if cwn_sense:
             sense_def=cwn_sense[2].encode('utf-8')
-            print wn_pos,
-            print p,
+#            print wn_pos,
+#            print p,
             print lemma_type,
             print ''.join(sense_def.split())
