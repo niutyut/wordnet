f33c57c10040857b35aefca2a7b2fe10f6167d9c
mhshih <d00142002@ntu.edu.tw>
2015-01-23 10:26:05 +0800

    Jseg example

--- stopwords.py
+++ stopwords.py
@@ -7,18 +7,16 @@ def lesk(example,kwic):
 kwics=[]
 stopwords=[w for w in open('stopwords.csv').read().split()]
 for line in open('lemma_definition_example.csv').readlines():
-    print '\n'.join(line.split(','))
-    print line
-    lemma,definition,example=line.split(',')
-    print lemma,',',definition,',',example.strip()
+#    print '\n'.join(line.split(','))
+#    print line
+    if len(line.split(','))==3:
+        lemma,definition,example=line.split(',')
+        print lemma,',',definition,',',#example.strip()
+        segmented=jieba.seg(example.strip()).nopos().split()
+        for w in segmented:
+            if w.encode('utf-8') not in stopwords:print w.encode('utf-8'),
+        print
 '''
-    segmented=jieba.seg(example.strip()).nopos().split()
-    for w in segmented:
-        if w.encode('utf-8') not in stopwords:
-            #print w.encode('utf-8'),
-            pass
-#    print
-
     for kwic in kwics:
         if lesk(example,kwic)>.5:
             print lemma,definition,''.join(kwic)
