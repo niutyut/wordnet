8b6f516e25872bcdda9b4fdcc7c761abd396a19d
mhshih <d00142002@ntu.edu.tw>
2015-01-30 11:22:57 +0800

    evaluate 100 rich lemmas popped

---
+++ manual_evaluate_popping_asbc_with_original_rich.py
@@ -0,0 +1,13 @@
+top100=list()
+for line in open('sorted_unpopped_debug_CWNMOE_lemmas.csv').readlines()[:100]:
+    lemma=line.split()[0][1:-1]
+    top100.append(lemma)
+
+i=0
+lines=open('popping_debug_CWNMOE_asbc.csv').readlines()
+while i<len(lines)-1:
+    s1,s2=lines[i],lines[i+1]
+    if s1.split()[0] in top100:
+        print s1.strip()
+        print s2.strip()
+    i+=2
--- sort_popped_lemma_by_sense_number.py
+++ sort_popped_lemma_by_sense_number.py
@@ -1,7 +1,7 @@
 i=0
-lines=open('popping.csv').readlines()
+for line in open('popped_debug_CWNMOE_asbc_100.csv'):
+    
+lines=open('popping_debug_CWNMOE_asbc.csv').readlines()
 while i<len(lines)-1:
     s1,s2=lines[i],lines[i+1]
-    print s1.split()[0]
-    print s2.split()[0]
     i+=2
