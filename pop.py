714ae1f0867c0c1a1a6de3b63062c30ff41a4608
mhshih <d00142002@ntu.edu.tw>
2015-01-31 00:14:29 +0800

    pre-build evaluation matrix

--- pop.py
+++ pop.py
@@ -32,11 +32,11 @@ for line in open('CWNMOE-def-ex.csv').readlines():
             flag=0
 #    if flag:d[lemma]={definition:[0]}
 
-eval_matrix={} # {lemma:[definitions]}
-#for lemma in d:
- #   for definition in lemma:
-  #      if lemma not in eval_matrix:eval_matrix[lemma]=[definition]
-   #     else:eval_matrix[lemma].append(definition)
+eval_matrix={} # {lemma:[[def1,history,...],[def2,history,...]]}
+for lemma in d:
+    for definition in lemma:
+        if lemma not in eval_matrix:eval_matrix[lemma]=[[definition]]
+        else:eval_matrix[lemma].append([definition])
 
 #for lemma in eval_matrix:
  #   for definition in eval_matrix[lemma]:print lemma,definition
@@ -45,10 +45,10 @@ for lemma in d:
     i=0
     while i<len(d[lemma].keys()):
         defi=d[lemma].keys()[i]
-        print lemma,defi
+#        print lemma,defi
 
-        if lemma not in eval_matrix:eval_matrix[lemma]=[defi]
-        else:eval_matrix[lemma].append(defi)
+#        if lemma not in eval_matrix:eval_matrix[lemma]=[[defi]]
+ #       else:eval_matrix[lemma].append([defi])
 
         j=0
         while j<i and j<len(d[lemma].keys()) and i<len(d[lemma].keys()):
@@ -58,12 +58,11 @@ for lemma in d:
                 if cos_similar(v1,v2)>.5:
 #                    print lemma,def1
  #                   print lemma,def2
-
                     k=0
                     for k in range(len(eval_matrix[lemma])):
-                        if eval_matrix[lemma][k].split(',')[0]==def1:
-                            eval_matrix[lemma][k]=def1+','+def2
-                        if eval_matrix[lemma][k].split(',')[0]==def2:
+                        if eval_matrix[lemma][k][0]==def1:
+                            eval_matrix[lemma][k].append(def2)
+                        elif eval_matrix[lemma][k][0]==def2:
                             eval_matrix[lemma].pop(k)
 
                     eval_matrix[lemma]
@@ -76,10 +75,9 @@ for lemma in d:
 #for lemma in d:
  #   for definition in d[lemma]:print lemma,definition
 
-for lemma in eval_matrix:
-    for definition in eval_matrix[lemma]:
-        if ',' in definition:
-            defis=definition.split(',')
+for lemma in eval_matrix: # {lemma:[[def1,history,...],[def2,history,...]]}
+    for defis in eval_matrix[lemma]:
+        if len(defis)>1:
             print defis[0],',',defis[0]
             for defi in defis[1:]:print defi
 
